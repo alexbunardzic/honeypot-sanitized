8f839897f0280d8c7a955541587731e742c01706
Paul Hammond <paul.hammond@gmail.com>
2026-04-23 18:16:03 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/form-sanitiser.js

---
+++ src/form-sanitiser.js
@@ -0,0 +1,11 @@
+module.exports = function sanitizeFormData(formData) {
+  if (formData === "") {
+    return "I do not accept an empty string";
+  }
+
+  if (formData.length > 15) {
+    return "I do not accept a string longer than 15 characters";
+  }
+
+  return "ok";
+};
